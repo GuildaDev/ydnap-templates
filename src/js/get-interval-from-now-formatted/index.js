dc3eb3fbfa0ab33369b6136a4ef5a70ac58eb793
Alex Oliveira <alexoliveira7x@gmail.com>
2025-05-01 00:02:11 +0100

    add new snippet

--- src/js/get-interval-from-now-formatted/index.js
+++ src/js/get-interval-from-now-formatted/index.js
@@ -4,11 +4,13 @@ export const INTERVAL_DATE_ERR =
 /**
  * Formats the time interval between the given date and the current date
  * into a human-readable relative time string.
+ *
+ * @param {Date} date - The target date to calculate the interval from now.
+ * @param {string} locale - A string with a BCP 47 language tag to specify the locale for formatting.
+ * @returns {string} A formatted string representing the relative time interval.
+ * @throws {TypeError} Throws an error if the provided date is not a valid Date object.
  */
-export default function getIntervalFromNowFormatted(
-  date: Date,
-  locale: Intl.LocalesArgument
-): string {
+export default function getIntervalFromNowFormatted(date, locale) {
   if (!(date instanceof Date) || isNaN(date.getTime())) {
     throw new TypeError(INTERVAL_DATE_ERR);
   }
