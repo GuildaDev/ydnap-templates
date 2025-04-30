4f929f545b047700660e6c7da344aea778c3e2ab
Alexandro Castro <10711649+alexcastrodev@users.noreply.github.com>
2025-04-30 18:11:35 +0100

    chore: change hooks name (#3)

--- src/ts/use-debounce-callback/use-debounce-callback.spec.ts
+++ src/ts/use-debounce-callback/use-debounce-callback.spec.ts
@@ -1,6 +1,6 @@
 import { expect, it, describe, vi, beforeEach } from 'vitest';
 import { renderHook } from '@testing-library/react';
-import useDebouncedCallback from '.';
+import useDebounceCallback from '.';
 
 const getSomeData = vi.fn(
   (id: number, quantity: number, options: { obj?: boolean }) => {
@@ -13,23 +13,23 @@ const getSomeData = vi.fn(
 );
 const debounceTime = 10;
 
-describe('useDebouncedCallback', () => {
+describe('useDebounceCallback', () => {
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
   it('Should debounce callbacks and execute the last one for each key', async () => {
     const { result } = renderHook(() =>
-      useDebouncedCallback(getSomeData, debounceTime)
+      useDebounceCallback(getSomeData, debounceTime)
     );
 
     // Call the debounced callback without key, it will debounce by default
-    result.current.debounced(1, 1, { obj: false });
-    result.current.debounced(1, 4, { obj: true });
-    result.current.debounced(1, 4, { obj: false });
-    result.current.debounced(1, 2, { obj: false });
-    result.current.debounced(1, 5, { obj: false });
-    result.current.debounced(1, 8, { obj: true });
+    result.current.debounce(1, 1, { obj: false });
+    result.current.debounce(1, 4, { obj: true });
+    result.current.debounce(1, 4, { obj: false });
+    result.current.debounce(1, 2, { obj: false });
+    result.current.debounce(1, 5, { obj: false });
+    result.current.debounce(1, 8, { obj: true });
 
     // Wait for the debounce time to elapse
     await new Promise((resolve) => setTimeout(resolve, debounceTime + 10));
@@ -40,16 +40,16 @@ describe('useDebouncedCallback', () => {
 
   it('Should not debounce if flush', async () => {
     const { result } = renderHook(() =>
-      useDebouncedCallback(getSomeData, 10000000)
+      useDebounceCallback(getSomeData, 10000000)
     );
 
     // Call the debounced callback without key, it will debounce by default
-    result.current.debounced(1, 1, { obj: false });
-    result.current.debounced(1, 4, { obj: true });
-    result.current.debounced(1, 4, { obj: false });
-    result.current.debounced(1, 2, { obj: false });
-    result.current.debounced(1, 5, { obj: false });
-    result.current.debounced(1, 8, { obj: true });
+    result.current.debounce(1, 1, { obj: false });
+    result.current.debounce(1, 4, { obj: true });
+    result.current.debounce(1, 4, { obj: false });
+    result.current.debounce(1, 2, { obj: false });
+    result.current.debounce(1, 5, { obj: false });
+    result.current.debounce(1, 8, { obj: true });
 
     result.current.flush();
 
@@ -59,16 +59,16 @@ describe('useDebouncedCallback', () => {
 
   it('Should cancel debounce callback', async () => {
     const { result } = renderHook(() =>
-      useDebouncedCallback(getSomeData, debounceTime)
+      useDebounceCallback(getSomeData, debounceTime)
     );
 
     // Call the debounced callback without key, it will debounce by default
-    result.current.debounced(1, 1, { obj: false });
-    result.current.debounced(1, 4, { obj: true });
-    result.current.debounced(1, 4, { obj: false });
-    result.current.debounced(1, 2, { obj: false });
-    result.current.debounced(1, 5, { obj: false });
-    result.current.debounced(1, 8, { obj: true });
+    result.current.debounce(1, 1, { obj: false });
+    result.current.debounce(1, 4, { obj: true });
+    result.current.debounce(1, 4, { obj: false });
+    result.current.debounce(1, 2, { obj: false });
+    result.current.debounce(1, 5, { obj: false });
+    result.current.debounce(1, 8, { obj: true });
 
     result.current.cancel();
 
