6932f710ae590319cb298118a784d1606ac59424
Alex Oliveira <alexoliveira7x@gmail.com>
2025-04-30 17:25:50 +0100

    chore: refactoring hook

--- src/ts/use-keyed-debounced-callback/index.ts
+++ src/ts/use-keyed-debounced-callback/index.ts
@@ -1,13 +1,46 @@
-export default function useKeyedDebouncedCallback<Payload>(
-  callback: (payload: Payload, key?: unknown) => void,
-  delay: number
-) {
-  // TODO: Implement the function
-  // I will receive the callback and the delay
-  // the function will be called with the payload and the key
-  // the function will debounce the callback by key
-  // if the key is not passed, it will debounce the callback by default ( maybe a symbol )
-  // if the key is passed, it will debounce the callback by key
-  // See the tests for more details
-  // I will use a Map to store the timeouts by key
+import { useRef } from 'react';
+
+type DebounceKey = string | symbol | number;
+const DEBOUNCE_ERR_KEY = 'Key is required for debouncing';
+
+/**
+ * A custom hook that returns a debounced function, which delays the execution
+ * of the provided callback until after a specified delay has elapsed since the
+ * last time it was invoked with the same key.
+ *
+ * @template T - The type of the callback function.
+ * @param callback - The function to be debounced. It will be executed after the
+ * delay period if no further calls are made with the same key.
+ * @param delay - The delay in milliseconds to wait before invoking the callback.
+ * @returns A debounced function that accepts a unique key and the arguments
+ * to pass to the callback. The key is used to manage separate debounce timers.
+ * 
+ * @throws {TypeError} If the provided key is not a valid primitive type (e.g.,
+ * string, number, or symbol).
+ */
+export default function useKeyedDebouncedCallback<
+  T extends (...args: any[]) => void,
+>(callback: T, delay: number) {
+  const timeouts = useRef(
+    new Map<DebounceKey, ReturnType<typeof setTimeout>>()
+  );
+
+  function debouncedFunction(key: DebounceKey, ...args: Parameters<T>) {
+    if (!key || ['object', 'function'].includes(typeof key)) {
+      throw new TypeError(DEBOUNCE_ERR_KEY);
+    }
+
+    if (timeouts.current.has(key)) {
+      clearTimeout(timeouts.current.get(key));
+    }
+
+    const timeout = setTimeout(() => {
+      callback(...args);
+      timeouts.current.delete(key);
+    }, delay);
+
+    timeouts.current.set(key, timeout);
+  }
+
+  return debouncedFunction;
 }
--- src/ts/use-keyed-debounced-callback/use-keyed-debounced-callback.spec.ts
+++ src/ts/use-keyed-debounced-callback/use-keyed-debounced-callback.spec.ts
@@ -1,44 +1,110 @@
-import { expect, it, describe, vi } from 'vitest';
+import { expect, it, describe, vi, beforeEach } from 'vitest';
 import { renderHook } from '@testing-library/react';
 import useKeyedDebouncedCallback from '.';
 
-describe('useKeyedDebouncedCallback', () => {
-  it('Should call many callbacks debounced by key', async () => {
-    const callback_1 = vi.fn();
-    const debounceTime = 1000;
+const getSomeData = vi.fn(
+  (id: number, quantity: number, options: { obj?: boolean }) => {
+    return {
+      id,
+      quantity,
+      ...options,
+    };
+  }
+);
+const debounceTime = 10;
 
+describe('useKeyedDebouncedCallback', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('Should call many callbacks and execute the last one', async () => {
     const { result } = renderHook(() =>
-      useKeyedDebouncedCallback(callback_1, debounceTime)
+      useKeyedDebouncedCallback(getSomeData, debounceTime)
     );
 
     // Call the debounced callback without key, it will debounce by default
-    result.current({ item_id: 1 });
-    result.current({ item_id: 2 });
-    result.current({ item_id: 3 });
+    result.current(Symbol.for('product_1'), 1, 1, { obj: false });
+    result.current(Symbol.for('product_1'), 1, 2, { obj: false });
+    result.current(Symbol.for('product_1'), 1, 3, { obj: true });
 
-    expect(callback_1).toHaveBeenCalledTimes(1);
-    // Should call the last callback
-    // The last call should be the one with item_id: 3
-    expect(callback_1).toHaveBeenNthCalledWith(1, { item_id: 3 });
-
-    // Wait for debounce time to pass
-    await new Promise((resolve) => setTimeout(resolve, debounceTime + 10));
-    // reset counter
-    callback_1.mockClear();
-
-    // passing key to the callback, it will debounce by key
-    result.current({ item_id: 1 }, 'key1');
-    result.current({ item_id: 1 }, 'key2');
-    result.current({ item_id: 2 }, 'key1');
-    result.current({ item_id: 4 }, 'key1');
-    result.current({ item_id: 2 }, 'key2');
-    result.current({ item_id: 3 }, 'key2');
-
-    // Wait for debounce time to pass
+    // Wait for the debounce time to elapse
     await new Promise((resolve) => setTimeout(resolve, debounceTime + 10));
 
-    expect(callback_1).toHaveBeenCalledTimes(2);
-    expect(callback_1).toHaveBeenNthCalledWith(1, { item_id: 4 }, ['key1']);
-    expect(callback_1).toHaveBeenNthCalledWith(2, { item_id: 3 }, ['key2']);
+    // Verify the callback was called once
+    expect(getSomeData).toHaveBeenCalledTimes(1);
+
+    // Should call the last callback, passing correct quantity
+    expect(getSomeData).toHaveBeenCalledWith(1, 3, { obj: true });
+  });
+
+  it('Should debounce callbacks by key and execute the last one for each key', async () => {
+    const { result } = renderHook(() =>
+      useKeyedDebouncedCallback(getSomeData, debounceTime)
+    );
+
+    // Call the debounced callback without key, it will debounce by default
+    result.current('key1', 1, 1, { obj: false });
+    result.current('key2', 1, 4, { obj: true });
+    result.current('key1', 1, 4, { obj: false });
+    result.current('key2', 1, 2, { obj: false });
+    result.current('key1', 1, 5, { obj: false });
+    result.current('key2', 1, 8, { obj: true });
+
+    // Wait for the debounce time to elapse
+    await new Promise((resolve) => setTimeout(resolve, debounceTime + 10));
+
+    expect(getSomeData).toHaveBeenCalledTimes(2);
+    expect(getSomeData).toHaveBeenCalledWith(1, 5, { obj: false });
+    expect(getSomeData).toHaveBeenCalledWith(1, 8, { obj: true });
+  });
+
+  it('Should fail if passing non valid key', async () => {
+    const { result } = renderHook(() =>
+      useKeyedDebouncedCallback(getSomeData, debounceTime)
+    );
+
+    // Call the debounced callback without key, it will debounce by default
+    expect(() => {
+      result.current('', 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      // @ts-expect-error - testing invalid key
+      result.current(null, 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      // @ts-expect-error - testing invalid key
+      result.current(undefined, 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      // @ts-expect-error - testing invalid key
+      result.current(false, 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      result.current(0, 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      // @ts-expect-error - testing invalid key
+      result.current({}, 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      // @ts-expect-error - testing invalid key
+      result.current([], 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    expect(() => {
+      // @ts-expect-error - testing invalid key
+      result.current(() => {}, 1, 1, { obj: false });
+    }).toThrowError(new TypeError('Key is required for debouncing'));
+
+    // Wait for the debounce time to elapse
+    await new Promise((resolve) => setTimeout(resolve, debounceTime + 10));
+    expect(getSomeData).toHaveBeenCalledTimes(0);
   });
 });
